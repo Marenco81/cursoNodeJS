6810085dc1782e1e7aa73a04849c2ffa1271b17f
Alvaro Marenco <alvaromarencom@gmail.com>
2025-01-31 12:58:28 -0600

    With promises

--- 4.fs-promise.js
+++ 4.fs-promise.js
@@ -1,16 +1,18 @@
-const fs = require('node:fs');
+const fs = require('node:fs/promises');
+const { text } = require('node:stream/consumers');
 
 console.log('Leyendo el primer archivo....');
-fs.readFile('./archivo.txt', 'utf-8', (err, text) =>{
-    console.log(text);
-});
+fs.readFile('./archivo.txt', 'utf-8')
+    .then(text => {
+        console.log(text)
+    });
 
 
 
 console.log('----> Hacer cosas mientras lee el codigo...')
 
 console.log('Leyendo el segundo archivo....');
-fs.readFile('./archivo2.txt', 'utf-8', (err, text) => {
-    console.log(text);
-});
-
+fs.readFile('./archivo2.txt', 'utf-8')
+    .then(text => {
+        console.log(text)
+    });
