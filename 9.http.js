cc178afdf913ff5db6ea3e258cc122416b2e50cc
Alvaro Marenco <alvaromarencom@gmail.com>
2025-02-05 18:06:47 -0600

    Segunda aplicacion - find available port

--- 9.http.js
+++ 9.http.js
@@ -1,11 +1,18 @@
 const http = require('node:http');
+const {findAvailablePort} = require('./10.free-port.js');
 
 const server = http.createServer((req, res) => {
     console.log('request received');
     res.end('Hola mundo');
 });
 
-server.listen(0, () => {
-    // console.log('Server listening on port 3000');
-    console.log(`server listening on port http://localhost:${server.address().port}`) // usando servidor '0' se asigna el primero libre
+findAvailablePort(2000).then(port => {
+    server.listen(port, () => {
+        console.log(`server listening on port http://localhost:${port}`);
+    })
 })
+
+// server.listen(0, () => {
+//     // console.log('Server listening on port 3000');
+//     console.log(`server listening on port http://localhost:${server.address().port}`) // usando servidor '0' se asigna el primero libre
+// })
